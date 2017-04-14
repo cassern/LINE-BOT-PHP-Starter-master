47d5f0673b2e0c0e32a5e83afeb79c0c452dac05
cassern <suk.jedsada@gmail.com>
2017-04-14 17:21:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,6 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
+				//jedsada
+				"text": "Hello"
 				'text' => $text
 			];
 
@@ -27,6 +29,11 @@ if (!is_null($events['events'])) {
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
+				//jedsada
+				"type":"text",
+            			"text":"May I help you?"
+				//jedsada
+        }
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -46,45 +53,3 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 
-
-
-
-
-
-{
-  "events": [
-      {
-        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-        "type": "message",
-        "timestamp": 1462629479859,
-        "source": {
-             "type": "user",
-             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-         },
-         "message": {
-             "id": "325708",
-             "type": "text",
-             "text": "Hello, world"
-          }
-      }
-  ]
-}
-
-
-
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
--d '{
-    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}' https://api.line.me/v2/bot/message/reply
